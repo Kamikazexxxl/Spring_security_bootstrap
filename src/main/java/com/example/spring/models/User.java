2d2422f3ddb4d16db68978ae4e794cfb292ab59d
Rustam <kamikazexxxl@gmail.com>
2022-07-16 03:00:59 +0300

    123

--- src/main/java/com/example/spring/models/User.java
+++ src/main/java/com/example/spring/models/User.java
@@ -15,96 +15,27 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
-@ToString
-//@Scope("session")
 @Entity
 @Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Setter
-    @Getter
     private long id;
-
-    @Column(nullable = true, unique = true)
-    @Setter
-    @Getter
-    @Size(min = 4, max = 20)
     private String username;
-
-    @Column(nullable = false)
-    @Size(min = 4, max = 20)
-    @Setter
-    @Getter
     private String name;
-
-    @Column/*(nullable = false)*/
-    @Setter
-    @Getter
-    @Size(min = 4, max = 20)
     private String surname;
-
-    @Column/*(nullable = false)*/
-    @Setter
-    @Getter
     private int age;
-
-    @Column(unique = true)
-    @Setter
-    @Getter
     private String email;
-
-    @Column(nullable = false)
-    @Setter
-    @Getter
-    @Size(min = 3)
     private String password;
-
-    @Column()
-    @ColumnDefault(value = "true")
-    @Getter
-    @Setter
     private Boolean accountNonExpired;
-
-    @Column()
-    @ColumnDefault(value = "true")
-    @Getter
-    @Setter
     private Boolean accountNonLocked;
-
-    @Column()
-    @ColumnDefault(value = "true")
-    @Getter
-    @Setter
     private Boolean credentialsNonExpired;
-
-    @Column()
-    @Getter
-    @Setter
-    @ColumnDefault(value = "true")
     private Boolean enabled;
-
-    @Getter
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
     private Set<Role> roles;
-
-    public User(String username, String name, String surname, int age, String email, String password, Boolean accountNonExpired, Boolean accountNonLocked, Boolean credentialsNonExpired, Boolean enabled, Set<Role> roles) {
-        this.username = username;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.email = email;
-        this.password = password;
-        this.accountNonExpired = accountNonExpired;
-        this.accountNonLocked = accountNonLocked;
-        this.credentialsNonExpired = credentialsNonExpired;
-        this.enabled = enabled;
-        this.roles = roles;
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles.stream().map(el -> new SimpleGrantedAuthority("ROLE_" + el.getAuthority())).collect(Collectors.toList());
